4f324add5e1aae96a1423c18be30f9a3c6eef0a2
Melody <polgn1995@gmail.com>
2019-08-12 11:54:16 +0200

    Bug: los espacios en blanco o casillas vacias crean errores

--- External/dataBlock.js
+++ External/dataBlock.js
@@ -7,20 +7,34 @@ function UserTemplate(){
 }
 
 var processUsersToSystem = function(usersList){
-    userAux = usersList.map(function(number){
-        padding = 2
-        userFinal = new UserTemplate()
-        
-        userFinal.surname = number[0].substring(0, number[0].lastIndexOf(','))
-        userFinal.name = number[0].substring(number[0].lastIndexOf(',')+padding, number[0].lastIndexOf('('))
-        userFinal.code = number[0].substring(number[0].lastIndexOf('(')+1, number[0].lastIndexOf(')'))
-        userFinal.import = number[1]
-        userFinal.reason = number[2]
 
-        return userFinal
+    userAuxAux = usersList.map(function(number){
+        if(number.length>=3 && number[0]!=null && number[1]!=null && number[2]!=null){
+
+            padding = 2
+            userFinal = new UserTemplate()
+            
+            userFinal.surname = number[0].substring(0, number[0].lastIndexOf(','))
+            userFinal.name = number[0].substring(number[0].lastIndexOf(',')+padding, number[0].lastIndexOf('('))
+            userFinal.code = number[0].substring(number[0].lastIndexOf('(')+1, number[0].lastIndexOf(')'))
+            userFinal.import = number[1]
+            userFinal.reason = number[2]
+
+            return userFinal
+        }
     })
 
-    //console.log(userAux)
+    var userAux=[]
+
+    for(var i=0;i<userAuxAux.length;i++){
+        if(userAuxAux[i]!=null){
+            console.log(userAuxAux[i])
+            userAux.push(userAuxAux[i])
+        }
+    }
+    
+    console.log("Limpio")
+    console.log(userAux)
     return userAux
 }
 
